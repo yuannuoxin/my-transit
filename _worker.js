2fc2b80120bcd55050fba508a35be1c86624b14f
yuannuoxin <3339694043@qq.com>
2024-06-03 19:29:59 +0800

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,13 @@
+export default {
+	async fetch(request, env) {
+		const url = new URL(request.url);
+		console.log('url:' + url);
+		const pathp = url.pathname.replace(/^\/|\/$/g, '');
+		console.log('pathp:' + pathp);
+		const addrPath = url.pathname.replace(/^\/|\/$/g, '');
+		console.log('addrPath:' + addrPath);
+		const newUrl = new URL('https://' + addrPath);
+		console.log('newUrl:' + newUrl);
+		return fetch(new Request(newUrl, request));
+	}
+};
