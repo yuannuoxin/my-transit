22d2e33cab44f1254ad41d22bc22f80978edc3ad
yuannuoxin <3339694043@qq.com>
2024-06-03 20:06:59 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,11 +2,14 @@ export default {
 	async fetch(request, env) {
 		const url = new URL(request.url);
 		console.log('url:' + url);
+		if (url.pathname === '/') {
+			return new Response(JSON.stringify(request.cf), { status: 200 });
+		}
 		const pathp = url.pathname.replace(/^\/|\/$/g, '');
 		console.log('pathp:' + pathp);
 		const addrPath = url.pathname.replace(/^\/|\/$/g, '');
 		console.log('addrPath:' + addrPath);
-		const newUrl = new URL('https://' + addrPath)+ url.search + url.hash;
+		const newUrl = new URL('https://' + addrPath);
 		console.log('newUrl:' + newUrl);
 		return fetch(new Request(newUrl, request));
 	}
